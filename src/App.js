edc186adeefd1b33d1e7c5025c3c86d96a4b0778
Askar Pulatov <master15994@gmail.com>
2023-10-04 08:48:09 +0300

    completed app with my own refactoring

--- src/App.js
+++ src/App.js
@@ -2,92 +2,93 @@ import React, { useEffect, useState } from 'react';
 import './index.scss';
 import { Collection } from './Collection';
 
-const categories = [
-  "Все",
-  "Море",
-  "Горы",
-  "Архитектура",
-  "Города"
-];
+const categ = [
+  {
+    "name": "Все"
+  },
+  {
+    "name": "Море"
+  },
+  {
+    "name": "Горы"
+  },
+  {
+    "name": "Архитектура"
+  },
+  {
+    "name": "Города"
+  }
+]
 
-const API_URL = 'https://651bc32b194f77f2a5aed3a1.mockapi.io/collection';
+const App = () => {
+  const [collections, setCollections] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+  const [page, setPage] = useState(1)
+  const [categoryId, setCategoryId] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
 
-function App() {
-  const [collections, setCollections] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
-  const [page, setPage] = useState(1);
-  const [categoryId, setCategoryId] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
+  const category = categoryId ? `category=${categoryId}` : ''
 
-  const category = categoryId ? `category=${categoryId}` : '';
+  const API_URL = 'https://651bc32b194f77f2a5aed3a1.mockapi.io/collection?'
+
+  const pageNumbers = [1, 2, 3, 4, 5]
+
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(`${API_URL}?page=${page}&limit=3&${category}`)
-      .then((res) => res.json())
+    setIsLoading(true)
+    fetch(`${API_URL}page=${page}&limit=3& ${category}`)
+      .then(res => res.json())
       .then((json) => {
-        setCollections(json);
+        setCollections(json)
       })
-      .catch((err) => {
-        console.error(err);
+      .catch(err => {
+        console.warn(err);
         alert('Ошибка');
       })
       .finally(() => {
-        setIsLoading(false);
-      });
-  }, [categoryId, page]);
-
-  const handleCategoryClick = (index) => {
-    setCategoryId(index);
-  };
-
-  const renderCollections = () => {
-    if (isLoading) {
-      return <h2>Идет загрузка....</h2>;
-    }
-
-    const filteredCollections = collections.filter((obj) =>
-      obj.name.toLowerCase().includes(searchValue.toLowerCase())
-    );
-
-    return filteredCollections.map((obj) => (
-      <Collection key={obj.name} name={obj.name} images={obj.photos} />
-    ));
-  };
+        setIsLoading(false)
+      })
+  }, [categoryId, page])
 
   return (
+
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          {categories.map((categoryName, index) => (
-            <li
-              key={categoryName}
-              className={categoryId === index ? 'active' : ''}
-              onClick={() => handleCategoryClick(index)}
-            >
-              {categoryName}
-            </li>
-          ))}
+          {
+            categ.map((obj, i) =>
+              <li
+                key={obj.name}
+                className={categoryId === i ? 'active' : ''}
+                onClick={() => setCategoryId(i)}
+              >
+                {obj.name}
+              </li>)
+          }
         </ul>
         <input
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
           className="search-input"
-          placeholder="Поиск по названию"
-        />
+          placeholder="Поиск по названию" />
       </div>
-      <div className="content">{renderCollections()}</div>
+      <div className="content">
+        {
+          isLoading ? <h2>Идет Загрузка....</h2> : (
+            collections
+              .filter((obj) => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
+              .map((obj, index) => <Collection key={index} name={obj.name} images={obj.photos} />))}
+      </div>
       <ul className="pagination">
-        {[...Array(5)].map((_, i) => (
+        {pageNumbers.map((pageNumber) => (
           <li
-            key={i}
-            className={page === i + 1 ? 'active' : ''}
-            onClick={() => setPage(i + 1)}
-          >
-            {i + 1}
-          </li>
-        ))}
+            key={pageNumber}
+            className={page === pageNumber ? 'active' : ''}
+            onClick={() => setPage(pageNumber)}
+          >{pageNumber}
+          </li>))
+        }
       </ul>
     </div>
   );
